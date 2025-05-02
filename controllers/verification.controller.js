30e85fa2ee17ea93f1c7e151deef1cb197e0ad40
Eriveltonssa <eriveltonmagalhaesdossantos@gmail.com>
2025-05-02 14:48:38 -0300

    feat: add teste de verificação com ia

--- controllers/verification.controller.js
+++ controllers/verification.controller.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const axios = require('axios');
 
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
-const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision:generateContent?key=${GEMINI_API_KEY}`;
+const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
